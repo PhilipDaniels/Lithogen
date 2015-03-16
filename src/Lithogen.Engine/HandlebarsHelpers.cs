a9b4cf23b3633f5e791a3a88b9229251c251b984
Philip Daniels <Philip.Daniels1971@gmail.com>
2015-03-16 19:29:11 +0000

    Add Hbs Helpers skeletons.

---
+++ src/Lithogen.Engine/HandlebarsHelpers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lithogen.Engine.HandlebarsHelpers
+{
+    public class CodeHelpers
+    {
+    }
+
+    public class CollectionHelpers
+    {
+    }
+
+    public class ComparisonHelpers
+    {
+    }
+
+    public class ContentHelpers
+    {
+    }
+
+    public class DataHelpers
+    {
+    }
+
+    public class DateHelpers
+    {
+    }
+
+    public class HtmlHelpers
+    {
+    }
+
+    public class InflectionHelpers
+    {
+    }
+
+    public class LoggingHelpers
+    {
+    }
+
+    public class MarkdownHelpers
+    {
+    }
+
+    public class MathHelpers
+    {
+    }
+
+    public class MiscellaneousHelpers
+    {
+    }
+
+    public class NumericHelpers
+    {
+    }
+
+    public class PathHelpers
+    {
+    }
+
+    public class StringHelpers
+    {
+    }
+
+    public class UrlHelpers
+    {
+    }
+}
